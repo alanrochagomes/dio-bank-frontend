4dbe3cde134a1aa79595fd8d00615286e4e3f764
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-24 08:16:27 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Layout } from "./components/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <h1>Faça o login</h1>
+      <label>Email</label>
+      <input />
+
+      <label>Senha</label>
+      <input />
+    </Layout>
+  );
+}
+
+export default App;
