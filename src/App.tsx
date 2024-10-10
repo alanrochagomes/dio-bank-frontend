88d302c008c3f718b5bb1ea6e8a694a6055d7336
Alan Rocha <alanrochaarg2001@gmail.com>
2024-10-10 09:29:35 -0300

    Alterando os valores no localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import "./pages/Home/Home.css";
 
 import { AppContextProvider } from "./components/AppContext";
 import MainRoutes from "./routes";
+import {
+  changeLocalStorage,
+  createLocalStorage,
+  getAllLocalStorage,
+} from "./services/storage";
 
 function App() {
+  createLocalStorage();
+
+  console.log(getAllLocalStorage());
+
+  changeLocalStorage({
+    login: true,
+  });
+
   return (
     <AppContextProvider>
       <MainRoutes />
